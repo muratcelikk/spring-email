5aa597bd1890c45f84865c10fd296309b9487288
Murat Çelik <muratcelik11@gmail.com>
2021-08-18 18:35:53 +0300

    via

--- src/main/java/com/cal/springemail/EmailSenderService.java
+++ src/main/java/com/cal/springemail/EmailSenderService.java
@@ -1,9 +1,15 @@
 package com.cal.springemail;
 
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 @AllArgsConstructor
@@ -11,7 +17,9 @@ public class EmailSenderService {
 
     private JavaMailSender mailSender;
 
-    public void sendEmail(String toEmail, String body, String subject) {
+    public void sendEmail(String toEmail,
+                          String body,
+                          String subject) {
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("muratcelik11@gmail.com");
@@ -21,5 +29,26 @@ public class EmailSenderService {
 
         mailSender.send(message);
         System.out.println("Mail Sent successfully...");
+    }
+
+    public void sendEmailWithAttachment(String toEmail,
+                                        String body,
+                                        String subject,
+                                        String pathToAttachment) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom("muratcelik11@gmail.com");
+        helper.setTo(toEmail);
+        helper.setText(body);
+        helper.setSubject(subject);
+
+        FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment(file.getFilename(), file);
+
+        mailSender.send(message);
+        System.out.println("Mail send..");
     }
 }
